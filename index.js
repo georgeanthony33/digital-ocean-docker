5689dca2abe4a14efcc896f656ecfa1f21453aa7
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-07 00:13:21 +0100

    github webhook endpoint changes

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 4567
-const bodyParser = require('body-parser')
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.get('/status', (req, res) => res.send({status: "I'm alive!"}))
 
 app.post('/payload', (req, res) => {
-  // const reqData = JSON.parse(req.body)
   console.log(res._events)
 })
 
