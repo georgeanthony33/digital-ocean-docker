af674434b4cfeeea3281c9fc3558bdaa60fb756b
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-06 23:33:22 +0100

    added github webhook endpoint

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4567
 
 app.get('/status', (req, res) => res.send({status: "I'm alive!"}))
+
+app.post('/payload', (req, res) => console.log(res))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
