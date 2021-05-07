10f8bbd0aa114b1602d574f1bc8650b7f87a42c5
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-07 12:20:54 +0100

    testing webhooks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4567
 const exec = require('sync-exec');
 
 app.get('/status', (req, res) => res.send({status: "I'm still alive!"}))
