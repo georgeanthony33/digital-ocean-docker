7bdeb705cb9eb06658d8cd69cc5e26efdec64538
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-10 18:20:48 +0100

    testing webhooks

--- webhook.js
+++ webhook.js
@@ -6,13 +6,14 @@ const exec = require('sync-exec');
 app.post('/payload', (req, res) => {
   if (req.headers['x-github-event'] === "push") {
     const dockerTag = req.headers['x-github-delivery']
-    exec('docker build . -t digital-ocean-docker')
-    exec(`docker tag digital-ocean-docker georgeanthony33/digital-ocean-docker:${dockerTag}`)
-    exec('docker login')
-    exec(`docker push georgeanthony33/digital-ocean-docker:${dockerTag}`)
-    exec('ssh -i id_rsa root@138.68.169.66')
-    exec('service docker restart')
-    exec(`docker run -p 3000:3000 georgeanthony33/digital-ocean-docker:${dockerTag}`)
+    console.log(dockerTag)
+    console.log(exec('docker build . -t digital-ocean-docker'))
+    console.log(exec(`docker tag digital-ocean-docker georgeanthony33/digital-ocean-docker:${dockerTag}`))
+    console.log(exec('docker login'))
+    console.log(exec(`docker push georgeanthony33/digital-ocean-docker:${dockerTag}`))
+    console.log(exec('ssh -i id_rsa root@138.68.169.66'))
+    console.log(exec('service docker restart'))
+    console.log(exec(`docker run -p 3000:3000 georgeanthony33/digital-ocean-docker:${dockerTag}`))
     console.log(exec('echo 1'))
   }
 })
