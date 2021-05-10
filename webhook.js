3809842082fb71ecc4a2b4e14222595a77a25c64
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-10 22:13:17 +0100

    testing webhooks

--- webhook.js
+++ webhook.js
@@ -7,9 +7,9 @@ require('dotenv').config()
 app.post('/payload', (req, res) => {
   if (req.headers['x-github-event'] === "push") {
     console.log(exec('docker build . -t digital-ocean-docker'))
-    console.log(exec("docker tag digital-ocean-docker georgeanthony33/digital-ocean-docker:2.0"))
+    console.log(exec("docker tag digital-ocean-docker georgeanthony33/digital-ocean-docker"))
     console.log(exec('docker login'))
-    console.log(exec("docker push georgeanthony33/digital-ocean-docker:2.0"))
+    console.log(exec("docker push georgeanthony33/digital-ocean-docker"))
     return res.status(200).send({ status: "done" })
   }
 })
