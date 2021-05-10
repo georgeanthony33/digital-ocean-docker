3c2f48f5e1d7cb1f84da5a1e2e123cc86b3e2372
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-10 20:27:26 +0100

    testing webhooks

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4567
 const exec = require('sync-exec');
-const exec = require('ssh-exec')
+const sshExec = require('ssh-exec')
 require('dotenv').config()
 
 app.post('/payload', (req, res) => {
@@ -14,7 +14,7 @@ app.post('/payload', (req, res) => {
     console.log(exec('docker login'))
     console.log(exec(`docker push georgeanthony33/digital-ocean-docker:${dockerTag}`))
     // console.log(exec('ssh -i id_rsa root@138.68.169.66'))
-    console.log(exec('service docker restart', 'root@138.68.169.66').pipe(process.stdout))
+    console.log(sshExec('service docker restart', 'root@138.68.169.66').pipe(process.stdout))
     // ssh.exec('service docker restart', { out: function (stdout) { console.log(stdout); } }).start()
     // ssh.exec(`docker run -p 3000:3000 georgeanthony33/digital-ocean-docker:1.9`, { out: function (stdout) { console.log(stdout); } }).start()
     console.log(exec('echo hello'))
