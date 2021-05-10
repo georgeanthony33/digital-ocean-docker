4f1604ba3bdc93040bd0112fa377465a9c96bc3b
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-10 21:45:37 +0100

    testing webhooks

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/status', (req, res) => res.send({status: "Hello world"}))
+app.get('/status', (req, res) => res.send({status: "Hello world!"}))
 
 app.listen(port, () => console.log(`Status app listening on port ${port}!`))
