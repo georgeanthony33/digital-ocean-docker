96be8a514200ffcb18ab8078cdcc28482cd55889
georgeanthony33 <georgeanthony33@gmail.com>
2021-05-10 20:25:19 +0100

    testing webhooks

--- webhook.js
+++ webhook.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 4567
-const exec = require('sync-exec');
-const SSH = require('simple-ssh');
+const exec = require('ssh-exec')
 require('dotenv').config()
 
 app.post('/payload', (req, res) => {
@@ -13,18 +12,8 @@ app.post('/payload', (req, res) => {
     console.log(exec(`docker tag digital-ocean-docker georgeanthony33/digital-ocean-docker:${dockerTag}`))
     console.log(exec('docker login'))
     console.log(exec(`docker push georgeanthony33/digital-ocean-docker:${dockerTag}`))
-    console.log(exec('ssh -i id_rsa root@138.68.169.66'))
-    const ssh = new SSH({
-      host: process.env.host,
-      user: process.env.user,
-      pass: process.env.pass,
-    });
-    ssh.exec('service docker restart', {
-      out: function(stdout) {
-          console.log(stdout)
-      }
-    }).start()
-    ssh.end()
+    // console.log(exec('ssh -i id_rsa root@138.68.169.66'))
+    console.log(exec('service docker restart', 'root@138.68.169.66').pipe(process.stdout))
     // ssh.exec('service docker restart', { out: function (stdout) { console.log(stdout); } }).start()
     // ssh.exec(`docker run -p 3000:3000 georgeanthony33/digital-ocean-docker:1.9`, { out: function (stdout) { console.log(stdout); } }).start()
     console.log(exec('echo hello'))
